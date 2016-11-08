c0d79ff92fdb4b3f9fb18fda8fbce34698c928b7
nazaretl <nazaretl@hu-berlin.de>
2016-11-08 19:07:16 +0100

    next chunk

--- _Qualifying/BondOnlyCoupon/BondOnlyCoupon.R
+++ _Qualifying/BondOnlyCoupon/BondOnlyCoupon.R
@@ -1,83 +1,3 @@
-# ---------------------------------------------------------------------
-# Book:        STF
-# ---------------------------------------------------------------------
-# See also:    BondOnlyCoupon, BondZeroCoupon, BondZeroCouponHPP
-# ---------------------------------------------------------------------
-# Quantlet:    BondCoupon
-# ---------------------------------------------------------------------
-# Description: BondCoupon computes price of the coupon-bearing CAT bond 
-#              for the given claim amount distribution and non-homogeneous 
-#              Poisson process governing the flow of losses 
-# ---------------------------------------------------------------------
-# Usage:       y = BondCoupon(Z,C,D,T,r,lambda,parlambda,distr,params,Tmax,N)
-# ---------------------------------------------------------------------
-# Input:       
-# Parameter:   Z
-# Definition:  scalar, payment at maturity
-# Parameter:   C
-# Definition:  scalar, coupon payments (cease at the threshold time or Tmax)
-# Parameter:   D
-# Definition:  n1 x 1 vector, threshold level 
-# Parameter:   T
-# Definition:  n2 x 1 vector, time to expiry 
-# Parameter:   r
-# Definition:  scalar, continuously-compounded discount rate 
-# Parameter:   lambda
-# Definition:  scalar, intensity function
-#              if lambda=0, a sine function
-#              if lambda=1, a linear function
-#              if lambda=2, a sine square function
-# Parameter:   parlambda
-# Definition:  n x 1 vector, parameters of the intensity function lambda
-#              (n=2 for lambda=1, n=3 otherwise)
-# Parameter:   distrib
-# Definition:  string, claim size distribution
-# Parameter:   params
-# Definition:  n x 1 vector, parameters of the claim size distribution
-#              n = 1 (exponential)
-#              n = 2 (gamma, lognormal, Pareto, Weibull)
-#              n = 3 (Burr, mixofexps)
-# Parameter:   Tmax
-# Definition:  scalar, time horizon
-# Parameter:   N
-# Definition:  scalar, number of trajectories
-# ---------------------------------------------------------------------
-# Output:      
-# Parameter:   y
-# Definition:  m x 3 matrix, the first column are times to bond's expiration,
-#              the second threshold levels and the third corresponding prices of the bond 
-# ---------------------------------------------------------------------
-# Example:     
-#             Z = 1
-#             C = 0.06
-#             D = c(1e9,2e9)
-#             T = c(1,2)
-#             r = log(1.025)
-#             lambda    = 0
-#             parlambda = c(39,14,-0.2)
-#             distr     = "Burr"
-#             params    = c(0.5,4*1e16,5) 
-#             Tmax      = max(T)
-#             N  = 20 
-#             d1 = BondCoupon(Z,C,D,T,r,lambda,parlambda,distr,params,Tmax,N)
-#             d1
-# ---------------------------------------------------------------------
-# Result:     Contents of d1
-#            [1,]        1    1e+09   1.0349 
-#            [2,]        1    2e+09   1.0349 
-#            [3,]        2    1e+09   1.0689 
-#            [4,]        2    2e+09   1.0689 
-# ---------------------------------------------------------------------
-# Keywords:   CAT bond 
-# ---------------------------------------------------------------------
-# Reference:  K. Burnecki, G. Kukla, D. Taylor (2005) "Pricing of catastrophe bonds ",
-#	          in "Statistical Tools for Finance and Insurance", 
-#             eds. P. Cizek, W. Härdle, R. Weron, Springer. 
-# ---------------------------------------------------------------------
-# Author:     Awdesch Melzer 20130629
-# ---------------------------------------------------------------------
-
-
 ########################## SUBROUTINES ##########################
 
 Burrrnd = function(alpha,lambda,tau,n,m){
@@ -233,6 +153,7 @@ simNHPP = function(lambda,parlambda,T,N){
     yy = yy[1:(2*maxEN+2),,]
     return(yy)
 }
+
 
 Paretornd = function(alpha,lambda,n,m){
 #PARETORND Random arrays from Pareto distribution.
@@ -474,57 +395,53 @@ simNHPPALP = function(lambda,parlambda,distrib,params,T,N){
 
 ########################## MAIN PROGRAM ##########################
 
-BondCoupon = function(Z,C,D,T,r,lambda,parlambda,distr,params,Tmax,N){
+BondOnlyCoupon = function(C,D,T,r,lambda,parlambda,distr,params,Tmax,N){
+
 
   if(lambda != 0 && lambda != 1 && lambda!=2){
-  	stop("BondCoupon: Lambda must be either 0,1 or 2.")
-  }
-  if(length(Z) !=1){
-  	stop("BondCoupon: payment at maturity Z needs to be a scalar")
+  	stop("BondOnlyCoupon: Lambda must be either 0,1 or 2.")
   }
   if(length(C) !=1){
-  	stop("BondCoupon: coupon payments C needs to be a scalar")
+  	stop("BondOnlyCoupon: coupon payments C needs to be a scalar")
   }
   if(length(r) !=1){
-  	stop("BondCoupon: discount rate needs to be a scalar")
+  	stop("BondOnlyCoupon: discount rate needs to be a scalar")
   }
   if(length(D)==1){
-  	stop("BondCoupon: threshold level D needs to be a vector ")
+  	stop("BondOnlyCoupon: threshold level D needs to be a vector ")
   }
   if(length(T)==1){
-  	stop("BondCoupon: time to expiry T needs to be a vector ")
+  	stop("BondOnlyCoupon: time to expiry T needs to be a vector ")
   }
-  x   = simNHPPALP(lambda,parlambda,distr,params,Tmax,N)
-  Tl  = length(T)
-  Dl  = length(D)
-  y   = matrix(0,Tl*Dl,3)
-  i   = 1 #loop (times to maturity)
-  j   = 1 #loop (treshold levels)
-  k   = 1 #loop (trajectories)
-  wyn  = 0
-  wyn2 = 0
+  x  = simNHPPALP(lambda,parlambda,distr,params,Tmax,N)
+  Tl = length(T)
+  Dl = length(D)
+  y  = matrix(0,Tl*Dl,3)
+  i  = 1 #loop (times to maturity)
+  j  = 1 #loop (treshold levels)
+  k  = 1 #loop (trajectories)
+  wyn= 0
   while(i<=Tl){
     while(j<=Dl){
       while(k<=N){
-        traj = cbind(x[,k,1],x[,k,2])
+        traj  = cbind(x[,k,1],x[,k,2])
         if (traj[length(traj[which(traj[,1]<=T[i]),1]),2]<=D[j]){
-          wyn  = wyn+(1-exp(-r*T[i]))/r
-          wyn2 = wyn2+1
+          wyn = wyn + (1-exp(-r*T[i]))/r
         }else{
-          wyn  = wyn+(1-exp(-r*traj[length(traj[which(traj[,2]<=D[j])]),1]))/r
+          wyn = wyn + (1-exp(-r*traj[length(traj[which(traj[,2]<=D[j])]),1]))/r
         }
         k = k + 1
       }
       y[(i-1)*Dl+j,1] = T[i]
       y[(i-1)*Dl+j,2] = D[j]
-      y[(i-1)*Dl+j,3] = C*wyn/N+Z*exp(-r*T[i])*wyn2/N 
-      wyn  = 0
-      wyn2 = 0
-      k    = 1
-      j    = j+1
+      y[(i-1)*Dl+j,3] = C*wyn/N 
+      wyn = 0
+      k   = 1
+      j   = j+1
     }
     j = 1
     i = i + 1
   }
   return(y)
 }
+
---
+++ _Qualifying/Burrrnd/Burrrnd.R
@@ -0,0 +1,24 @@
+Burrrnd = function(alpha,lambda,tau,n,m){
+	
+  if (missing(m)){
+    m      = 1
+  }
+  if (missing(n)){
+    n      = 1
+  }
+  if (missing(tau)){
+    tau    = 2
+  }
+  if (missing(lambda)){
+    lambda = 1
+  }
+  if (missing(alpha)){
+    alpha  = 1
+  }
+   u = matrix(0,n,m)
+   for (i in 1:m){
+   	   u[,i] = (lambda*(runif(n,0,1)^(-1/alpha)-1))^(1/tau)
+   }
+   y = u
+return(y)
+}
